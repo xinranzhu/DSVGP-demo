56d18f13ae665a092852cfc5a1fc7e76e3ae7191
Xinran Zhu <xz584@cornell.edu>
2023-04-28 21:26:06 -0400

    add refs

--- tests/plot_testfun.py
+++ tests/plot_testfun.py
@@ -1,3 +1,8 @@
+# refs:
+# https://stackoverflow.com/questions/9170838/surface-plots-in-matplotlib
+# https://www.tutorialspoint.com/matplotlib/matplotlib_contour_plot.htm
+# https://docs.gpytorch.ai/en/stable/examples/08_Advanced_Usage/Simple_GP_Regression_Derivative_Information_2d.html
+
 def plot_testfun(train_x, train_y, gp_model):
     # subplot 1
     # 1) plot the contour/surface of the underlying testfun
@@ -10,7 +15,6 @@ def plot_testfun(train_x, train_y, gp_model):
 
     # subplot 2
     # 1) plot the gp prediction (predictive mean) use gp_model 
-
 
 
     # subplot 3 
